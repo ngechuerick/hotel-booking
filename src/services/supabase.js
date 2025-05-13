241cf912e5e87afcb1a27ea4e4cef4357b13f81a
ngechuerick <erick.ngechu3@gmail.com>
2025-05-13 10:54:30 +0300

    secret fix

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://luyfbsblgucgyftnytkh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1eWZic2JsZ3VjZ3lmdG55dGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMjUxNTgsImV4cCI6MjA2MjYwMTE1OH0.H5J-GGLyMI1xTK6sdgf5Tqq2KxIDmvGTugSagzl1k7s";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+console.log(supabaseKey);
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
